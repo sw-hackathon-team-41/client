2189f04c05b8b5e8295c4e42a14746471ce41576
LulzCo <ysw991106@gmail.com>
2023-06-29 21:25:59 +0900

    배너 적용

--- src/components/bannerButton.jsx
+++ src/components/bannerButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function BannerButton() {
     return (
         <>
-            <button className="btn bg-white text-lime-900 font-bold text-xl top-3/4 left-10 w-64 h-20 rounded-lg ">테스트하기</button>
+            <button className="btn bg-white text-lime-900 font-bold text-xl absolute top-2/3 left-20 w-40 h-20 rounded-lg ">테스트하기</button>
         </>
     )
 }
