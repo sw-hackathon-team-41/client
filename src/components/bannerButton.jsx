1c6bdf33c4e89bf1442b41d473c467bf52f7c511
LulzCo <ysw991106@gmail.com>
2023-06-29 10:30:03 +0900

    배서 생성

--- src/components/bannerButton.jsx
+++ src/components/bannerButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function BannerButton() {
     return (
         <>
-            <button className="btn bg-white text-lime-900 font-bold text-xl absolute top-3/4 left-10 w-64 h-20 rounded-lg ">테스트하기</button>
+            <button className="btn bg-white text-lime-900 font-bold text-xl top-3/4 left-10 w-64 h-20 rounded-lg ">테스트하기</button>
         </>
     )
 }
